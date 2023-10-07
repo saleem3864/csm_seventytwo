38ad57bcaffb9701dba43832641ffd191f8a0c59
Saleem Abdullah <saleem.abdullah@logic-unit.com>
2023-10-08 01:17:44 +0500

    first commit

---
+++ data/data.ts
@@ -0,0 +1,27 @@
+const data = {
+  sitename: "SMC72",
+  sitetagline: "Comming Soon",
+  sitelogo: "",
+  title: "CSM72 - Comming Soon",
+  description:
+    "We&apos;re currently working 👨‍💻 hard to bring you something great, and we can&apos;t wait to share it with you 📅. Our team is putting the finishing touches on a new project 🚀 that we think you&apos;ll love 😍.",
+  newsletterheading: "Stay tuned for our live updates!",
+  copyrightText: "Copyright © 2023",
+  socialIconsHeading: "Follow Us 📣",
+  socialIcons: [
+    {
+      icon: "facebook",
+      link: "#",
+    },
+    {
+      icon: "twitter",
+      link: "#",
+    },
+    {
+      icon: "linkedIn",
+      link: "#",
+    },
+  ],
+};
+
+export default data;
